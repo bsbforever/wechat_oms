bbc8002c3ce8b557286ce1d9a5a92165b6d0738f
bsbforever <bsbforever520@hotmail.com>
2017-07-06 15:46:08 +0800

    commit

--- checkindex.py
+++ checkindex.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 #coding=utf8
 import cx_Oracle
+import pymysql
 from sendmail_phone import *
 
-def getindex(cursor):
+def getindex(oracle_cursor):
     fp=open('/home/oracle/script/getindex.sql','r')
     fp1=fp.read()
-    s=cursor.execute(fp1)
+    s=oracle_cursor.execute(fp1)
     fp.close()
     result=[]
     row=s.fetchall()
@@ -19,21 +20,37 @@ if __name__=="__main__":
     mailcontent=[] 
     ipaddress='10.65.1.120'
     username='sys'
-    password='sys_password'
+    password='ase_sys_n'
     port='1521'
     tnsname='dctest'
     #这里我们利用Python的异常处理来捕获异常，具体用法请参考文章开始提到的教程
     try:
         #这里我们使用sysdba权限连接oracle数据库(和上期连接普通用户的不同)
-        db = cx_Oracle.connect(username+'/'+password+'@'+ipaddress+':'+port+'/'+tnsname ,mode=cx_Oracle.SYSDBA)
+        oracle = cx_Oracle.connect(username+'/'+password+'@'+ipaddress+':'+port+'/'+tnsname ,mode=cx_Oracle.SYSDBA)
     except Exception as e:
         content= (tnsname+' is Unreachable,The reason is '+ str(e)).strip()
         print (content)
     else:
-        cursor = db.cursor()
-        data=getindex(cursor)
-        cursor.close()
-        db.close()
-        for i in data:
-            print (i) 
+        oracle_cursor = oracle.cursor()
+        data=getindex(oracle_cursor)
+        oracle_cursor.close()
+        oracle.close()
+
+     
+        mysql = pymysql.connect("localhost","root","Oracle@123","oracle" )
+        mysql_cursor = mysql.cursor()
+        for index in data:
+            checkifexist='select count(*) from oracle_indexmonitor where index_name=\''+index+'\''
+            mysql_cursor.execute(checkifexist)
+            count = mysql_cursor.fetchone()
+            if int(count[0])==0:
+                try:
+                    insertsql='insert into oracle_indexmonitor(index_name,ipaddress,tnsname) values(\''+index+'\',\''+ipaddress+'\',\''+tnsname+'\')'
+                    mysql_cursor.execute(insertsql)
+                    mysql.commit()
+                except:
+                    mysql.rollback()
+
+        mysql_cursor.close()
+        mysql.close()
 
